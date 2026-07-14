05f4bf55b26930b8c841f7a1cd064ae1524d4309
Ludiferr <sh342841@ucf.edu>
2026-07-13 20:11:14 -0400

    Add new multiplayer menu flow

--- Assets/DeadmansTales/Scripts/UI/MainMenuManager.cs
+++ Assets/DeadmansTales/Scripts/UI/MainMenuManager.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
-
+using TMPro;
 public class MainMenuManager : MonoBehaviour
 {
     public GameObject mainMenuPanel;
-    public GameObject playMenuPanel;
-    public GameObject multiplayerMenuPanel;
     public GameObject levelSelectPanel;
-    public GameObject lobbyRoomPanel;
+    public GameObject multiplayerPanel;
+    public GameObject connectionOptions;
+    public GameObject clientOptions;
+    public GameObject hostOptions;
+    public GameObject joinCodeOptions;
+    public TMP_InputField lobbyCodeInput;
 
     private void Start()
     {
@@ -16,48 +19,66 @@ public class MainMenuManager : MonoBehaviour
     public void ShowMainMenu()
     {
         mainMenuPanel.SetActive(true);
-        playMenuPanel.SetActive(false);
-        multiplayerMenuPanel.SetActive(false);
         levelSelectPanel.SetActive(false);
-        lobbyRoomPanel.SetActive(false);
-    }
-
-    public void ShowPlayMenu()
-    {
-        mainMenuPanel.SetActive(false);
-        playMenuPanel.SetActive(true);
-        multiplayerMenuPanel.SetActive(false);
-        levelSelectPanel.SetActive(false);
-        lobbyRoomPanel.SetActive(false);
-    }
-
-    public void ShowMultiplayerMenu()
-    {
-        mainMenuPanel.SetActive(false);
-        playMenuPanel.SetActive(false);
-        multiplayerMenuPanel.SetActive(true);
-        levelSelectPanel.SetActive(false);
-        lobbyRoomPanel.SetActive(false);
+        multiplayerPanel.SetActive(false);
     }
 
     public void ShowLevelSelectMenu()
     {
         mainMenuPanel.SetActive(false);
-        playMenuPanel.SetActive(false);
-        multiplayerMenuPanel.SetActive(false);
         levelSelectPanel.SetActive(true);
-        lobbyRoomPanel.SetActive(false);
+        multiplayerPanel.SetActive(false);
     }
 
-    public void ShowLobbyRoom()
+    public void ShowMultiplayerMenu()
     {
         mainMenuPanel.SetActive(false);
-        playMenuPanel.SetActive(false);
-        multiplayerMenuPanel.SetActive(false);
         levelSelectPanel.SetActive(false);
-        lobbyRoomPanel.SetActive(true);
+        multiplayerPanel.SetActive(true);
+        ShowConnectionOptions();
     }
 
+    public void ShowConnectionOptions()
+    {
+        connectionOptions.SetActive(true);
+        joinCodeOptions.SetActive(false);
+        clientOptions.SetActive(false);
+        hostOptions.SetActive(false);
+
+    }
+
+    public void ShowJoinCodeOptions()
+    {
+        connectionOptions.SetActive(false);
+        joinCodeOptions.SetActive(true);
+        clientOptions.SetActive(false);
+        hostOptions.SetActive(false);
+    }
+
+    // Temporary
+    public void PreviewHostLobby()
+    {
+        connectionOptions.SetActive(false);
+        joinCodeOptions.SetActive(false);
+        clientOptions.SetActive(false);
+        hostOptions.SetActive(true);
+    }
+
+    // Temporary 
+    public void PreviewClientLobby()
+    {
+        connectionOptions.SetActive(false);
+        joinCodeOptions.SetActive(false);
+        clientOptions.SetActive(true);
+        hostOptions.SetActive(false);
+    }
+
+
+    public void LeaveLobby()
+    {
+        ShowMultiplayerMenu();
+        // Network disconnect needs to be added. 
+    }
     public void ExitGame()
     {
         Debug.Log("Exit");
